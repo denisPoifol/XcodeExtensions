adb27050af3d623a89f785fb931031939ab5868e
Denis Poifol <denis.poifol@applidium.com>
2017-08-21 22:19:34 +0200

    Replace selections with TODO

--- XcodeExtension/TODO/SourceEditorCommand.swift
+++ XcodeExtension/TODO/SourceEditorCommand.swift
@@ -13,25 +13,67 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
 
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
         let comment = todoString()
-        guard let selections = invocation.buffer.selections as? [XCSourceTextRange] else {
-                completionHandler(nil)
-                return
-        }
-        for selection in selections {
-            append(comment, atTheEndOf: selection, in: invocation)
+        for selectionObject in invocation.buffer.selections {
+            guard let selection = selectionObject as? XCSourceTextRange else { continue }
+            replace(selection, in: invocation.buffer, with: comment)
         }
         completionHandler(nil)
     }
 
-    // MARK: - private methods
+    // MARK - private methods
 
-    private func append(_ comment: String, atTheEndOf selection: XCSourceTextRange, in invocation: XCSourceEditorCommandInvocation) {
-        guard var lastSelectionLine = invocation.buffer.lines[selection.end.line] as? String else {
-            return
+    private func replace(_ selection: XCSourceTextRange, in buffer: XCSourceTextBuffer, with text: String) {
+        if selection.start.line == selection.end.line {
+            let line = selection.start.line
+            guard var lineToChange = buffer.lines[line] as? String else { return }
+            let subRangeStartIndex = lineToChange.index(lineToChange.startIndex, offsetBy: selection.start.column)
+            guard selection.end.column > selection.start.column else {
+                lineToChange.insert(contentsOf: text.characters, at: subRangeStartIndex)
+                buffer.lines[selection.start.line] = lineToChange
+                return
+            }
+            let subRangeEndIndex = lineToChange.index(lineToChange.startIndex, offsetBy: selection.end.column - 1)
+            lineToChange.replaceSubrange(subRangeStartIndex...subRangeEndIndex, with: text)
+            buffer.lines[selection.start.line] = lineToChange
+        } else if selection.start.line + 1 == selection.end.line {
+            guard
+                let firstLine = buffer.lines[selection.start.line] as? String,
+                let lastLine = buffer.lines[selection.end.line] as? String
+                else { return }
+            if selection.end.column == 0 {
+                replace(
+                    XCSourceTextRange(
+                        start: selection.start,
+                        end: XCSourceTextPosition(
+                            line: selection.start.line,
+                            column: firstLine.characters.count
+                        )
+                    ),
+                    in: buffer, with: text)
+            }
+            let groupedLines = firstLine.appending(lastLine).replacingOccurrences(of: "\n", with: "")
+            let newSelection = XCSourceTextRange(
+                start: selection.start,
+                end: XCSourceTextPosition(
+                    line: selection.start.line,
+                    column: groupedLines.characters.count + selection.end.column - lastLine.characters.count + 1
+                )
+            )
+            buffer.lines.removeObject(at: selection.end.line)
+            buffer.lines.replaceObject(at: selection.start.line, with: groupedLines)
+            replace(newSelection, in: buffer, with: text)
+        } else {
+            let newSelection = XCSourceTextRange(
+                start: selection.start,
+                end: XCSourceTextPosition(
+                    line: selection.start.line + 1,
+                    column: selection.end.column
+                )
+            )
+            let indexSet: IndexSet = IndexSet(integersIn: (selection.start.line + 1)...(selection.end.line - 1))
+            buffer.lines.removeObjects(at: indexSet)
+            replace(newSelection, in: buffer, with: text)
         }
-        let endOfSelectionIndex = lastSelectionLine.index(lastSelectionLine.startIndex, offsetBy: selection.end.column)
-        lastSelectionLine.insert(contentsOf: comment.characters, at: endOfSelectionIndex)
-        invocation.buffer.lines[selection.end.line] = lastSelectionLine
     }
 
     private func todoString() -> String {
