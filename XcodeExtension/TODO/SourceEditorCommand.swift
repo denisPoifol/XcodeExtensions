52bc0685de283bd27494bc376f284d970a3befa2
Denis Poifol <denis.poifol@applidium.com>
2017-08-21 21:47:50 +0200

    Create XcodeEditorExtension target

---
+++ XcodeExtension/TODO/SourceEditorCommand.swift
@@ -0,0 +1,20 @@
+//
+//  SourceEditorCommand.swift
+//  TODO
+//
+//  Created by Denis Poifol on 21/08/2017.
+//  Copyright © 2017 Denis Poifol. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
